57f2d67399f80f5daaf6196c45661703acffb6d5
Giedo Stolk <stolkie@gmail.com>
2025-01-29 13:12:55 +0100

    Verie 0.1.13

--- bot/scalping_bot.py
+++ bot/scalping_bot.py
@@ -14,7 +14,7 @@ import json
 
 
 class ScalpingBot:
-    VERSION = "0.1.11"
+    VERSION = "0.1.13"
 
     def __init__(self, config: dict, logger: LoggingFacility, state_managers: dict, bitvavo, args: argparse.Namespace):
         self.config = config
--- run.sh
+++ run.sh
@@ -37,6 +37,6 @@ echo "🐳 Running Docker container '${IMAGE}_${TAG}_${CONFIG}'..."
 docker run --restart=always --name "${IMAGE}_${TAG}_${CONFIG}" -d \
   -v "$(pwd)/config/:/app/config" \
   -v "${VOLUME_NAME}:/app/data" \
-  "${IMAGE}:${TAG}" --config config/${CONFIG}.json --bot-name ${BOTNAME} || handle_error "Failed to start Docker container '${IMAGE}_${TAG}_${CONFIG}'."
+  "${IMAGE}:${TAG}" --config config/${CONFIG}.json --bot-name ${BOTNAME} || handle_error "Failed to start Docker container '${IMAGE}_${CONFIG}'."
 
-echo "✅ Docker container '${IMAGE}_${TAG}_${CONFIG}' is running successfully."
+echo "✅ Docker container '${IMAGE}_${CONFIG}' is running successfully."
