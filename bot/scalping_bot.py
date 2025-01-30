b512dab2c98539d755632a35f5de7abe130eb808
Giedo Stolk <stolkie@gmail.com>
2025-01-30 07:37:46 +0100

    Versie 0.1.15

--- bot/scalping_bot.py
+++ bot/scalping_bot.py
@@ -14,7 +14,7 @@ import json
 
 
 class ScalpingBot:
-    VERSION = "0.1.14"
+    VERSION = "0.1.15"
 
     def __init__(self, config: dict, logger: LoggingFacility, state_managers: dict, bitvavo, args: argparse.Namespace):
         self.config = config
