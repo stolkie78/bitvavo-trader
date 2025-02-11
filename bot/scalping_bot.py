40c1ab91bf14778c0011d47ce3450aaff17bd8a4
Giedo Stolk <stolkie@gmail.com>
2025-02-11 16:31:20 +0100

    Deployment en bugfix

--- bot/scalping_bot.py
+++ bot/scalping_bot.py
@@ -17,7 +17,7 @@ class ScalpingBot:
     """
     Asynchroon ScalpingBot met ondersteuning voor multi-posities en stop loss.
     """
-    VERSION = "0.1.23"
+    VERSION = "0.1.24"
 
     def __init__(self, config: dict, logger: LoggingFacility, state_managers: dict, bitvavo, args: argparse.Namespace):
         """
