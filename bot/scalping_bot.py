4f338d7e79fb802ead066acdd3f56e707e9fdf16
Giedo Stolk <stolkie@gmail.com>
2025-02-16 18:41:28 +0100

    Fix in de sell logic

--- bot/scalping_bot.py
+++ bot/scalping_bot.py
@@ -18,7 +18,7 @@ class ScalpingBot:
     """
     Async Scalping Bot with dynamic stoploss and risk allocation.
     """
-    VERSION = "0.4.7"
+    VERSION = "0.4.8"
 
     def __init__(self, config: dict, logger: LoggingFacility, state_managers: dict, bitvavo, args: argparse.Namespace):
         """
