c250169d70039d257df7b758d3f050f5e2b58001
Giedo Stolk <stolkie@gmail.com>
2025-03-05 08:59:28 +0100

    Version 0.5.15

--- bot/trader.py
+++ bot/trader.py
@@ -16,7 +16,7 @@ class Trader:
     """
     Async Scalping bot
     """
-    VERSION = "0.1.36"
+    VERSION = "0.1.37"
 
     def __init__(self, config: dict, logger: LoggingFacility, state_managers: dict, bitvavo, args: argparse.Namespace):
         """
